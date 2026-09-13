8e44e1d89728617078e46cd90cb8ebdbbaca666e
Aora-group-entreprise <hushengniaina@gmail.com>
2026-09-13 14:49:08 +0300

    apply Yunikofinal responsive foundation

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './yuniko.css'
+import './yuniko-responsive.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
